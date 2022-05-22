9ff875c1eb76df1bff11f01e3c24df8624cb9490
Mel Incognito <melxincognito@gmail.com>
2022-05-22 13:49:26 -0700

    fixed custom fonts by adding it to document instead of the header

--- components/navigation/Header.js
+++ components/navigation/Header.js
@@ -4,10 +4,6 @@ const Header = ({ Title }) => {
   return (
     <Head>
       <title> {Title}</title>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Libre+Bodoni&display=swap"
-        rel="stylesheet"
-      />
     </Head>
   );
 };
