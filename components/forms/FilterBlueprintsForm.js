21edd753c15bd57007011e1c19b338647a994840
Mel Incognito <melxincognito@gmail.com>
2022-05-22 13:43:45 -0700

    outlined

--- components/forms/FilterBlueprintsForm.js
+++ components/forms/FilterBlueprintsForm.js
@@ -1,8 +1,20 @@
-import { Card, Button, Typography, TextField } from "@mui/material";
+import * as React from "react";
+import { Card, Button, Typography } from "@mui/material";
 
-const FilterBlueprintsForm = () => {
+export default function FilterBlueprintsForm() {
+  const [sqfInput, setSqfInput] = React.useState("");
+
+  const handleSqfInputChange = (e) => {
+    setSqfInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(sqfInput);
+  };
+
   return (
-    <div>
+    <>
       <Card
         sx={{
           display: "grid",
@@ -12,15 +24,16 @@ const FilterBlueprintsForm = () => {
           borderRadius: 2,
         }}
       >
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="bedroomSelection">
             <Typography variant="h6"> # of Bedrooms</Typography>
-            <Button>1 </Button>
+            <Button>1</Button>
             <Button>2</Button>
             <Button>3</Button>
             <Button>4</Button>
             <Button>5</Button>
           </div>
+
           <div className="bathroomSelection">
             <Typography variant="h6"> # of Bathrooms</Typography>
             <Button>1 </Button>
@@ -34,26 +47,32 @@ const FilterBlueprintsForm = () => {
               <Button>3.5</Button>
             </div>
           </div>
+
           <div className="storiesSelection">
             <Typography variant="h6"> # of Stories</Typography>
             <Button>1 </Button>
             <Button>2</Button>
             <Button>3</Button>
           </div>
+
           <div className="garageSelection">
             <Typography variant="h6"> # of Garages</Typography>
             <Button>1 </Button>
             <Button>2</Button>
             <Button>3</Button>
           </div>
+
           <div className="sqFtSelection">
             <Typography variant="h6"> Sq Ft:</Typography>
-            <input />
+            <input onChange={handleSqfInputChange} />
           </div>
+
+          <Button variant="contained" sx={{ marginTop: 1 }} type="submit">
+            {" "}
+            Browse Blueprints
+          </Button>
         </form>
       </Card>
-    </div>
+    </>
   );
-};
-
-export default FilterBlueprintsForm;
+}
