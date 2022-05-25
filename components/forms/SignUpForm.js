c42c9b85f63ee5d43e046d2e3995f3bda99afcd0
Mel Incognito <melxincognito@gmail.com>
2022-05-24 17:41:00 -0700

    made Textfields into array objects

--- components/forms/SignUpForm.js
+++ components/forms/SignUpForm.js
@@ -8,12 +8,61 @@ import {
 } from "@mui/material";
 
 export default function SignUpForm() {
+  const [signUpFirstName, setSignUpFirstName] = useState("");
+  const [signUpLastName, setSignUpLastName] = useState("");
+  const [signUpCompanyName, setSignUpCompanyName] = useState("");
   const [signUpEmail, setsignUpEmail] = useState("");
   const [signUpPassword, setSignUpPassword] = useState("");
   const [confirmSignUpPassword, setConfirmSignUpPassword] = useState("");
 
+  const userSignUpInformation = [
+    {
+      label: "First Name",
+      onChangeFunc: setSignUpFirstName,
+      index: 0,
+    },
+    {
+      label: "Last Name",
+      onChangeFunc: setSignUpLastName,
+      index: 1,
+    },
+    {
+      label: "Company Name",
+      onChangeFunc: setSignUpCompanyName,
+      index: 2,
+    },
+  ];
+  // the email will be what's used for the login, and the sign up information is just for
+  // tracking personal information of users
+
+  const userSignupLoginInformation = [
+    {
+      label: "Email",
+      type: "email",
+      onChangeFunc: setsignUpEmail,
+      index: 0,
+      id: "emailInput",
+    },
+    {
+      label: "Password",
+      type: "password",
+      onChangeFunc: setSignUpPassword,
+      index: 1,
+      id: "signupPasswordInput",
+    },
+    {
+      label: "Confirm Password",
+      type: "password",
+      onChangeFunc: setConfirmSignUpPassword,
+      index: 2,
+      id: "confirmSignupPasswordInput",
+    },
+  ];
+
   const logit = (e) => {
     e.preventDefault();
+    console.log("Name: " + signUpFirstName + " " + signUpLastName);
+    console.log("Company Name: " + signUpCompanyName);
     console.log("email: " + signUpEmail);
     console.log("password: " + signUpPassword);
     console.log("confirm password: " + confirmSignUpPassword);
@@ -24,60 +73,61 @@ export default function SignUpForm() {
     boxShadow: "0 0.25rem 0.75rem rgba(0, 0, 0, 0.5)",
   };
 
-  const contentStyle = {
-    display: "grid",
+  const headerStyle = {
+    display: "flex",
     justifyContent: "center",
-    gap: "1rem",
+    marginTop: "1rem",
   };
+  const contentStyle = {
+    display: "flex",
+    justifyContent: "center",
+  };
+
   return (
     <>
       {" "}
       <Card className="loginCard" sx={cardStyles}>
-        <CardContent sx={contentStyle}>
-          <div
-            className="header"
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              marginTop: "1rem",
-            }}
-          >
-            <Typography variant="h5"> Sign Up </Typography>
-          </div>
-
-          <hr size="1" width="100%" color="gray" />
+        <CardContent sx={headerStyle}>
+          <Typography variant="h5"> Sign Up </Typography>
+        </CardContent>
+        <hr size="1" width="100%" color="gray" />
+        <CardContent>
           <form style={contentStyle} onSubmit={logit}>
-            <TextField
-              variant="outlined"
-              label="Email"
-              onChange={(e) => {
-                setsignUpEmail(e.target.value);
-              }}
-            />
-            <TextField
-              id="signupPasswordInput"
-              variant="outlined"
-              label="Password"
-              type="password"
-              onChange={(e) => {
-                setSignUpPassword(e.target.value);
-              }}
-            />{" "}
-            <TextField
-              id="confirmSignupPasswordInput"
-              variant="outlined"
-              label="Password"
-              type="password"
-              onChange={(e) => {
-                setConfirmSignUpPassword(e.target.value);
-              }}
-            />
-            <Button variant="contained" type="submit">
-              {" "}
-              Sign Up
-            </Button>
+            <div>
+              {userSignUpInformation.map((item) => (
+                <TextField
+                  key={item.index}
+                  variant="outlined"
+                  label={item.label}
+                  onChange={(e) => {
+                    item.onChangeFunc(e.target.value);
+                  }}
+                  fullWidth
+                />
+              ))}
+            </div>
+            <div>
+              {userSignupLoginInformation.map((item) => (
+                <TextField
+                  key={item.index}
+                  variant="outlined"
+                  label={item.label}
+                  type={item.type}
+                  id={item.id}
+                  onChange={(e) => {
+                    item.onChangeFunc(e.target.value);
+                  }}
+                  fullWidth
+                />
+              ))}
+            </div>
           </form>
         </CardContent>
+
+        <Button fullWidth variant="contained" type="submit" onClick={logit}>
+          {" "}
+          Sign Up
+        </Button>
       </Card>
     </>
   );
