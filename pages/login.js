6ab5a9625386b30f6bf268e52d71a16909c88991
Mel Incognito <melxincognito@gmail.com>
2022-05-24 19:18:35 -0700

    changed anchor tag to Link component

--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,30 @@
-import { Card, Typography } from "@mui/material";
+import LoginForm from "../components/forms/LoginForm";
+import { Card, CardContent, Typography } from "@mui/material";
+import Link from "next/link";
 
 function login() {
+  const cardStyles = {
+    padding: 2,
+    borderRadius: 2,
+    boxShadow: "0 0.25rem 0.75rem rgba(0, 0, 0, 0.5)",
+  };
+
   return (
     <div>
-      <Card sx={{ padding: "2rem", backgroundColor: "secondary.main" }}>
-        {" "}
-        Login
-      </Card>
-      <Card sx={{ padding: "2rem", backgroundColor: "secondary.main" }}>
-        <Typography>Login</Typography>
-      </Card>
+      <LoginForm />
+      <div style={{ marginTop: "1rem" }}>
+        <Card sx={cardStyles}>
+          <CardContent>
+            <Typography>
+              {" "}
+              Dont have an account?{" "}
+              <Link href="/signup" color="purple">
+                Click here to sign up!
+              </Link>{" "}
+            </Typography>{" "}
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
