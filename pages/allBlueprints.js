1b3aca3440a7b1b05df015e13e0c0005d3fbf226
Mel Incognito <melxincognito@gmail.com>
2022-06-06 18:42:53 -0700

    saving 2 transitions for later

--- pages/allBlueprints.js
+++ pages/allBlueprints.js
@@ -1,5 +1,37 @@
+import React from "react";
+import { motion } from "framer-motion";
+
 const allBlueprints = () => {
-  return <div>all the blueprints</div>;
+  //
+  //
+
+  return (
+    <div style={{ display: "flex" }}>
+      {/* transition slides down from the top of the page and 
+          springs up to its final position
+        */}
+      <motion.div
+        style={{ float: "left" }}
+        initial={{ y: -250 }}
+        animate={{ y: -10 }}
+        transition={{ delay: 0.2, type: "spring", stiffness: 120 }}
+      >
+        <h1>All the blueprints</h1>
+      </motion.div>
+      {/* transition slides up from the bottom of the page and 
+          springs down to its final position
+        */}
+
+      <motion.div
+        style={{ float: "right" }}
+        initial={{ y: 250 }}
+        animate={{ y: 10 }}
+        transition={{ delay: 0.2, type: "spring", stiffness: 120 }}
+      >
+        <h1>All the blueprints II</h1>
+      </motion.div>
+    </div>
+  );
 };
 
 export default allBlueprints;
