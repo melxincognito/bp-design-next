7ac1019fd312b5444da7d5813ef5c793a7f608ff
Mel Incognito <melxincognito@gmail.com>
2022-06-06 21:46:39 -0700

    added framer-motion transition

--- pages/customplanrequest.js
+++ pages/customplanrequest.js
@@ -1,10 +1,16 @@
 import React from "react";
 import CustomPlanRequestForm from "../components/forms/CustomPlanRequestForm";
+import { motion } from "framer-motion";
 
 export default function customplanrequest() {
   return (
-    <div>
+    <motion.div
+      transition={{ delay: 0.17 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
       <CustomPlanRequestForm />
-    </div>
+    </motion.div>
   );
 }
