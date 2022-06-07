e68a329cc821de623e7db8e587f7b19214c84177
Mel Incognito <melxincognito@gmail.com>
2022-06-06 22:03:13 -0700

    added framer motion initial transition

--- pages/signup.js
+++ pages/signup.js
@@ -1,9 +1,16 @@
+import React from "react";
 import SignUpForm from "../components/forms/SignUpForm";
+import { motion } from "framer-motion";
 
 export default function signup() {
   return (
-    <>
+    <motion.div
+      transition={{ delay: 0.17 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
       <SignUpForm />
-    </>
+    </motion.div>
   );
 }
