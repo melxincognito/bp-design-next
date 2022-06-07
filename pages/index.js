c199074adf9027f1c1ea03a2f7203eb9c5e2ad81
Mel Incognito <melxincognito@gmail.com>
2022-06-07 10:30:48 -0700

    added individual framer motion transitions to components

--- pages/index.js
+++ pages/index.js
@@ -14,37 +14,43 @@ export default function Home() {
     display: { xs: "flex", md: "none" },
   };
   return (
-    <motion.div
-      initial={{ x: -650 }}
-      animate={{ x: 0 }}
-      transition={{ delay: 0.2, type: "spring", stiffness: 100 }}
-    >
+    <motion.div>
       {/*Desktop page view */}
       <Box className="desktop-container" sx={desktopContainerStyles}>
-        <Box
-          className="header-image-container"
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            padding: "1rem",
-          }}
+        <motion.div
+          transition={{ delay: 0.17 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
         >
-          <img
-            src="https://images.unsplash.com/photo-1564501049412-61c2a3083791?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832"
-            width="90%"
-            height="100%"
-          />
           <Box
-            id="blueprints-form-container"
+            className="header-image-container"
             sx={{
-              position: "absolute",
-              right: "15%",
-              top: "26%",
+              display: "flex",
+              justifyContent: "center",
+              padding: "1rem",
             }}
           >
-            <FilterBlueprintsForm />
-          </Box>{" "}
-        </Box>
+            <img
+              src="https://images.unsplash.com/photo-1564501049412-61c2a3083791?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832"
+              width="90%"
+              height="100%"
+            />
+            <motion.div
+              initial={{ x: "-150vh" }}
+              animate={{ x: 0 }}
+              transition={{ delay: 2, type: "spring", stiffness: 100 }}
+              id="blueprints-form-container"
+              style={{
+                position: "absolute",
+                right: "15%",
+                top: "26%",
+              }}
+            >
+              <FilterBlueprintsForm />
+            </motion.div>{" "}
+          </Box>
+        </motion.div>
 
         <Box sx={{ backgroundColor: "blue" }}>
           <Typography> box 2</Typography>
