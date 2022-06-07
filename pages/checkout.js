80cba344c315a676141de743b762fa376c975faf
Mel Incognito <melxincognito@gmail.com>
2022-06-06 21:39:37 -0700

    added framer motion initial transition;

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,5 +1,15 @@
-function mycart() {
-  return <div>checkout</div>;
-}
+import React from "react";
+import { motion } from "framer-motion";
 
-export default mycart;
+export default function checkout() {
+  return (
+    <motion.div
+      transition={{ delay: 0.17 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      checkout
+    </motion.div>
+  );
+}
