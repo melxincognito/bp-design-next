5965cd82781fff9f810cc37982003056fb412cc8
Mel Incognito <melxincognito@gmail.com>
2022-06-08 21:02:37 -0700

    made general skeleton

--- pages/browsebpbystyle/spanish.js
+++ pages/browsebpbystyle/spanish.js
@@ -1,21 +1,71 @@
 import React from "react";
-import { Button } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import Link from "next/link";
+import {
+  Typography,
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Button,
+  CardActionArea,
+  CardActions,
+} from "@mui/material";
+import { motion } from "framer-motion";
 
 export default function spanishcolonial() {
+  const styleSelectionContainerStyles = {
+    display: "flex",
+    flexWrap: "wrap",
+    gap: 10,
+    backgroundColor: "rgba(0, 0, 0, 0.13)",
+    height: "100%",
+    width: "100%",
+    border: "solid black 15px",
+    borderStyle: "inset",
+    borderRadius: 1,
+    justifyContent: "center",
+    alignContent: "center",
+    padding: "3rem",
+  };
   return (
-    <div style={{ display: "flex", gap: 30 }}>
+    <div style={{ display: "flex" }}>
       <div>
         <Link href="/browsebpbystyle" passHref>
           <Button>
-            <ArrowBackIcon /> GO BACK
+            <ArrowBackIcon />
           </Button>
         </Link>
       </div>
-      <div>
-        <h3> Spanish </h3>
-      </div>
+      <motion.div
+        className="mainContainer"
+        transition={{ delay: 0.17 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        style={{ display: "grid", width: "100%", gap: "1rem" }}
+      >
+        <div
+          style={{
+            display: "grid",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+            textAlign: "center",
+          }}
+        >
+          <Typography variant="h3"> Spanish Style Homes</Typography>
+          <hr width="100%" />
+        </div>
+        <Box
+          className="stylesSelectionContainer"
+          sx={styleSelectionContainerStyles}
+        >
+          <Card>
+            <Typography> Hey </Typography>
+          </Card>
+        </Box>
+      </motion.div>
     </div>
   );
 }
