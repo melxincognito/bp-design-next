a9900cea1af15667a1d30925b8c9448bfeaf4a79
Mel Incognito <melxincognito@gmail.com>
2022-06-08 20:25:35 -0700

    creating skeleton of style selection page

--- pages/browsebpbystyle/index.js
+++ pages/browsebpbystyle/index.js
@@ -1,16 +1,44 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { Typography, Box } from "@mui/material";
 
 export default function browsebpbystyle() {
   return (
     <motion.div
+      className="mainContainer"
       transition={{ delay: 0.17 }}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
+      style={{ display: "grid" }}
     >
-      <div style={{ display: "flex", gap: 10 }}>
+      <div
+        style={{
+          display: "grid",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          textAlign: "center",
+        }}
+      >
+        <Typography variant="h3"> Browse Blueprints by Style</Typography>
+        <hr width="100%" />
+      </div>
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: 10,
+          backgroundColor: "pink",
+          height: "20rem",
+          width: "100%",
+          border: "solid black 20px",
+          borderRadius: 2,
+          justifyContent: "center",
+          alignContent: "center",
+        }}
+      >
         {stylesOptions.map((option) => (
           <>
             <button key={option.index}>
@@ -20,7 +48,7 @@ export default function browsebpbystyle() {
             </button>
           </>
         ))}
-      </div>
+      </Box>
     </motion.div>
   );
 }
