fec27116eb9541c8363471f35322831ce2c030e2
Mel Incognito <melxincognito@gmail.com>
2022-06-10 15:01:40 -0700

    added key specs

--- pages/browsebpbystyle/cabin/[planNumber].js
+++ pages/browsebpbystyle/cabin/[planNumber].js
@@ -3,6 +3,12 @@ import { Box, Typography } from "@mui/material";
 import { useRouter } from "next/router";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KingBedOutlinedIcon from "@mui/icons-material/KingBedOutlined";
+import BathroomOutlinedIcon from "@mui/icons-material/BathroomOutlined";
+import SquareFootIcon from "@mui/icons-material/SquareFoot";
+import GarageOutlinedIcon from "@mui/icons-material/GarageOutlined";
+import StairsOutlinedIcon from "@mui/icons-material/StairsOutlined";
+
 export default function CabinPlanDetails() {
   const router = useRouter();
 
@@ -51,29 +57,17 @@ export default function CabinPlanDetails() {
           <img
             src="https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2074&q=80"
             alt="house"
-            width="70%"
+            width="90%"
             height="70%"
             style={{
               boxShadow: "5px 5px 15px 5px rgba(0,0,0,0.3)",
               borderRadius: 2,
             }}
           />
-          <Box
-            sx={{
-              display: "flex",
-              gap: "4rem",
-              justifyContent: "flex-start",
-              alignItems: "center",
-            }}
-          >
+          <Box>
             <Typography variant="h4">
               {" "}
               Plan # <a style={{ color: "purple" }}>{planNumber} </a>{" "}
-            </Typography>
-            -
-            <Typography variant="h6">
-              {" "}
-              Style: <a style={{ color: "purple" }}>Cabin </a>
             </Typography>
           </Box>
         </Box>
@@ -85,7 +79,7 @@ export default function CabinPlanDetails() {
             <KeyboardArrowUpIcon />
           </Box>
           <img
-            src="https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2074&q=80"
+            src="https://images.unsplash.com/photo-1484154218962-a197022b5858?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2674&q=80"
             alt="house"
             width="135px"
             height="135px"
@@ -95,7 +89,7 @@ export default function CabinPlanDetails() {
             }}
           />
           <img
-            src="https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2074&q=80"
+            src="https://images.unsplash.com/photo-1449844908441-8829872d2607?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGhvbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60"
             alt="house"
             width="135px"
             height="135px"
@@ -105,7 +99,7 @@ export default function CabinPlanDetails() {
             }}
           />
           <img
-            src="https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2074&q=80"
+            src="https://images.unsplash.com/photo-1513584684374-8bab748fbf90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2665&q=80"
             alt="house"
             width="135px"
             height="135px"
@@ -127,15 +121,40 @@ export default function CabinPlanDetails() {
         className="container2-keyfeatures"
         sx={{
           display: "grid",
-          backgroundColor: "pink",
+          border: "5px inset black",
+          backgroundColor: "primary.main",
+          padding: 2,
           width: "70%",
+          color: "white",
+          boxShadow: "0px 0px 15px 5px rgba(0,0,0,0.28)",
         }}
       >
         <Box>
           <Typography variant="h4"> Key Specs</Typography>
         </Box>
-        <hr width="100%" />
-        <Box></Box>
+        <hr width="100%" color="white" />
+        <Box sx={{ display: "flex", gap: "2rem" }}>
+          <div>
+            <KingBedOutlinedIcon fontSize="large" />
+            <Typography variant="body1"> 3 beds</Typography>
+          </div>
+          <div>
+            <BathroomOutlinedIcon fontSize="large" />
+            <Typography variant="body1"> 3.5 Baths</Typography>
+          </div>
+          <div>
+            <GarageOutlinedIcon fontSize="large" />
+            <Typography variant="body1"> 3 Garages</Typography>
+          </div>
+          <div>
+            <StairsOutlinedIcon fontSize="large" />
+            <Typography variant="body1"> 2 Stories</Typography>
+          </div>{" "}
+          <div sx={{ display: "flex" }}>
+            <SquareFootIcon fontSize="large" />
+            <Typography variant="body1"> 1000 SqFt</Typography>
+          </div>
+        </Box>
       </Box>
 
       <h2> For Cabin</h2>
