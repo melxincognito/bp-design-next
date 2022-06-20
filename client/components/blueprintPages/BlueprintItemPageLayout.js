dd0050a79db4cbc2d58256f127da564bf93e4d6b
Mel Incognito <melxincognito@gmail.com>
2022-06-20 15:16:46 -0700

    changed image gallery view

--- client/components/blueprintPages/BlueprintItemPageLayout.js
+++ client/components/blueprintPages/BlueprintItemPageLayout.js
@@ -20,6 +20,7 @@ import GarageOutlinedIcon from "@mui/icons-material/GarageOutlined";
 import StairsOutlinedIcon from "@mui/icons-material/StairsOutlined";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ImageGallery from "./ImageGallery";
 
 export default function BlueprintItemPageLayout(props) {
   const [planSetOptions, setPlanSetOptions] = useState("");
@@ -60,29 +61,8 @@ export default function BlueprintItemPageLayout(props) {
   const imagesContainerStyles = {
     display: "flex",
     justifyContent: "center",
-    width: "70%",
-    alignItems: "center",
-  };
-
-  const imagePreviewContainerStyles = {
-    display: "grid",
-    justifyContent: "center",
-    alignItems: "center",
-    justifyItems: "center",
-  };
-
-  const toggleImageGalleryContainerStyles = {
-    display: "flex",
-    justifyContent: "center",
     justifyItems: "center",
     alignItems: "center",
-    boxShadow: "0px 0px 15px 5px rgba(0,0,0,0.14)",
-    borderRadius: 2,
-    "&:hover": {
-      backgroundColor: "highlight.main",
-      cursor: "pointer",
-      boxShadow: "0px 0px 15px 5px rgba(0,0,0,0.24)",
-    },
   };
 
   const descriptionsAddToCartContainerStyles = {
@@ -167,7 +147,7 @@ export default function BlueprintItemPageLayout(props) {
     display: "flex",
     justifyContent: "center",
   };
-
+  // DONT FORGET TO ADD props.planNumber SOMEWHERE YOU JUST DELETED IT FROM THE IMAGE PREVIEW THING
   return (
     <Box className="contentContainer" sx={contentContainerStyles}>
       <Box className="navigateBackContainer" sx={{ width: "100%" }}>
@@ -182,66 +162,7 @@ export default function BlueprintItemPageLayout(props) {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
       >
-        <Box className="imagePreviewContainer" sx={imagePreviewContainerStyles}>
-          <img
-            src="https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2074&q=80"
-            alt="house"
-            width="90%"
-            height="70%"
-            style={{
-              boxShadow: "5px 5px 15px 5px rgba(0,0,0,0.3)",
-              borderRadius: 2,
-            }}
-          />
-          <Box>
-            <Typography variant="h4"> Plan # {props.planNumber} </Typography>
-          </Box>{" "}
-        </Box>
-        <Box sx={{ display: "grid", gap: "1rem" }}>
-          <Box
-            className="toggleImageGalleryContainer"
-            sx={toggleImageGalleryContainerStyles}
-          >
-            <KeyboardArrowUpIcon />
-          </Box>
-          <img
-            src="https://images.unsplash.com/photo-1484154218962-a197022b5858?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2674&q=80"
-            alt="house"
-            width="135px"
-            height="135px"
-            style={{
-              boxShadow: "5px 5px 15px 5px rgba(0,0,0,0.3)",
-              borderRadius: 2,
-            }}
-          />
-          <img
-            src="https://images.unsplash.com/photo-1449844908441-8829872d2607?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGhvbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60"
-            alt="house"
-            width="135px"
-            height="135px"
-            style={{
-              boxShadow: "5px 5px 15px 5px rgba(0,0,0,0.3)",
-              borderRadius: 2,
-            }}
-          />
-          <img
-            src="https://images.unsplash.com/photo-1513584684374-8bab748fbf90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2665&q=80"
-            alt="house"
-            width="135px"
-            height="135px"
-            style={{
-              boxShadow: "5px 5px 15px 5px rgba(0,0,0,0.3)",
-              borderRadius: 2,
-            }}
-          />
-
-          <Box
-            className="toggleImageGalleryContainer"
-            sx={toggleImageGalleryContainerStyles}
-          >
-            <KeyboardArrowDownIcon />
-          </Box>
-        </Box>
+        <ImageGallery />
       </motion.div>
 
       <motion.div
