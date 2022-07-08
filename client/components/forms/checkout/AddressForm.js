02974d6823082ea86589788a5f6c8199a23a0765
Mel Incognito <melxincognito@gmail.com>
2022-07-08 14:41:06 -0700

    refactored mui component imports

--- client/components/forms/checkout/AddressForm.js
+++ client/components/forms/checkout/AddressForm.js
@@ -1,9 +1,11 @@
 import React from "react";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
+import {
+  Grid,
+  Typography,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
 
 export default function AddressForm() {
   return (
