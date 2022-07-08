584ea206a455738e7258ec84349849384e1a654a
Mel Incognito <melxincognito@gmail.com>
2022-07-08 14:42:33 -0700

    refactored mui component imports

--- client/components/forms/checkout/Review.js
+++ client/components/forms/checkout/Review.js
@@ -1,9 +1,6 @@
 import React from "react";
-import Typography from "@mui/material/Typography";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import Grid from "@mui/material/Grid";
+
+import { Typography, List, ListItem, ListItemText, Grid } from "@mui/material";
 
 const products = [
   {
