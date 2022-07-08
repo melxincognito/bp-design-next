efa5ec6419831d4fde4581554da26179f1e5e19f
Mel Incognito <melxincognito@gmail.com>
2022-07-08 14:41:54 -0700

    refactored mui component imports

--- client/components/forms/checkout/PaymentForm.js
+++ client/components/forms/checkout/PaymentForm.js
@@ -1,9 +1,11 @@
 import React from "react";
-import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
+import {
+  Typography,
+  Grid,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
 
 export default function PaymentForm() {
   return (
