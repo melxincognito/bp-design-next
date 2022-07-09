96e6560a9908e3d578e8b52ac2280a519eac2be7
Mel Incognito <melxincognito@gmail.com>
2022-07-09 12:05:29 -0700

    added error catching to display loading page if cart items havent been fetched yet

--- client/pages/checkout.js
+++ client/pages/checkout.js
@@ -21,6 +21,8 @@ import {
   ListItemText,
 } from "@mui/material";
 
+// TODO check why only the tax amount is getting sent to paypal
+
 const Transition = forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
@@ -29,32 +31,29 @@ export default function checkout() {
   const router = useRouter();
   const [openDialog, setOpenDialog] = useState(false);
   const [cartItems, setCartItems] = useState([]);
-  const [cartTotal, setCartTotal] = useState(1.03);
-  const [taxTotal, setTaxTotal] = useState(0);
-  const [cartPlusTaxTotal, setCartPlusTaxTotal] = useState(1.03);
-  // cart total is set to 1.03 because if it's at 0 to start there is an error idk why.
+  const [cartTotal, setCartTotal] = useState(null);
+  const [taxTotal, setTaxTotal] = useState(null);
+  const [cartPlusTaxTotal, setCartPlusTaxTotal] = useState(null);
+
   const getTotal = () => {
     let total = 0;
+    let taxTotalI = 0;
+    let cartPlusTaxTotalI = 0;
+    let salesTax = 0.17;
+    let cityTax = 0.04;
+    let stateTax = 0.09;
+
     cartItems.map((item) => {
       total += item.price;
     });
+
     setCartTotal(total);
-    return total;
-  };
+    taxTotalI = total * cityTax + total * stateTax + total * salesTax;
+    cartPlusTaxTotalI = cartTotal + taxTotalI;
+    setTaxTotal(taxTotalI.toFixed(2));
+    setCartPlusTaxTotal(cartPlusTaxTotalI.toFixed(2));
 
-  const calculateTax = () => {
-    // TODO CHECK WHAT OTHER TAXES NEED TO GET IMPLEMENTED INTO THE CALCULATION
-    let cityTax = 0.5;
-    let stateTax = 0.2;
-    let itemsTotal = cartTotal;
-    let taxTotal = itemsTotal * cityTax + itemsTotal * stateTax;
-    let total = 0;
-
-    total = taxTotal + itemsTotal;
-
-    setTaxTotal(taxTotal.toFixed(2));
-
-    setCartPlusTaxTotal(total.toFixed(2));
+    return total, taxTotalI, cartPlusTaxTotalI;
   };
 
   useEffect(() => {
@@ -62,7 +61,6 @@ export default function checkout() {
       try {
         setCartItems(response.data);
         getTotal();
-        calculateTax();
       } catch (error) {
         console.log(error);
       }
@@ -82,7 +80,7 @@ export default function checkout() {
       purchase_units: [
         {
           amount: {
-            value: cartTotal,
+            value: cartPlusTaxTotal,
           },
         },
       ],
@@ -94,6 +92,21 @@ export default function checkout() {
       handleClickOpenDialog();
     });
   };
+
+  var pricingItems = [
+    {
+      label: "Cart",
+      price: cartTotal,
+    },
+    {
+      label: "Tax",
+      price: taxTotal,
+    },
+    {
+      label: "Total",
+      price: cartPlusTaxTotal,
+    },
+  ];
 
   return (
     <>
@@ -128,42 +141,39 @@ export default function checkout() {
             Checkout
           </Typography>
           <hr width="100%" />
-          <List disablePadding>
-            {cartItems.map((item, index) => (
-              <ListItem key={index} sx={{ py: 1, px: 0 }}>
-                <ListItemText primary={"Plan #" + item.plan_number} />
-                <Typography variant="body2">${item.price}</Typography>
-              </ListItem>
-            ))}
 
-            <ListItem sx={{ py: 1, px: 0 }}>
-              <ListItemText primary="Cart" sx={{ fontWeight: 700 }} />
-              <Typography
-                variant="subtitle1"
-                sx={{ fontWeight: 700, color: "highlight.dark" }}
-              >
-                ${cartTotal}
-              </Typography>
-            </ListItem>
-            <ListItem sx={{ py: 1, px: 0 }}>
-              <ListItemText primary="Tax" sx={{ fontWeight: 700 }} />
-              <Typography
-                variant="subtitle1"
-                sx={{ fontWeight: 700, color: "highlight.dark" }}
-              >
-                ${taxTotal}
-              </Typography>
-            </ListItem>
-            <ListItem sx={{ py: 1, px: 0 }}>
-              <ListItemText primary="Total" sx={{ fontWeight: 700 }} />
-              <Typography
-                variant="subtitle1"
-                sx={{ fontWeight: 700, color: "highlight.dark" }}
-              >
-                ${cartPlusTaxTotal}
-              </Typography>
-            </ListItem>
-          </List>
+          {cartItems.length === 0 ? (
+            <Typography component="div" sx={{ textAlign: "center" }}>
+              Loading cart items . . .
+            </Typography>
+          ) : (
+            <List disablePadding>
+              {cartItems.map((item, index) => (
+                <ListItem key={index} sx={{ py: 1, px: 0 }}>
+                  <ListItemText primary={"Plan #" + item.plan_number} />
+                  <Typography variant="body2">${item.price}</Typography>
+                </ListItem>
+              ))}
+
+              {pricingItems.map((item, index) => {
+                return (
+                  <ListItem key={index} sx={{ py: 1, px: 0 }}>
+                    <ListItemText
+                      primary={item.label}
+                      sx={{ fontWeight: 700 }}
+                    />
+                    <Typography
+                      variant="subtitle1"
+                      sx={{ fontWeight: 700, color: "highlight.dark" }}
+                    >
+                      {item.price < 0 ? "$0.00" : "$" + `${item.price}`}
+                    </Typography>
+                  </ListItem>
+                );
+              })}
+            </List>
+          )}
+
           <hr width="100%" />
           <div>
             <PayPalScriptProvider
