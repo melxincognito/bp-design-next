d52b5f1348607be8dd07888e108740d10f5fdc44
Mel Incognito <melxincognito@gmail.com>
2022-07-08 17:01:28 -0700

    added paypal payment integration for testing. payments showing as unsuccessful but are still going through

--- client/pages/admindashboard.js
+++ client/pages/admindashboard.js
@@ -1,6 +1,110 @@
-import React, { useState, useReducer } from "react";
+import React, { useState, useReducer, useEffect } from "react";
 import Axios from "axios";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
+export default function admindashboard() {
+  const [show, setShow] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [ErrorMessage, setErrorMessage] = useState("");
+  const [orderID, setOrderID] = useState(false);
+
+  // creates a paypal order
+  const createOrder = (data, actions) => {
+    return actions.order
+      .create({
+        purchase_units: [
+          {
+            description: "Item",
+            amount: {
+              currency_code: "USD",
+              value: 70,
+            },
+          },
+        ],
+        // not needed if a shipping address is actually needed
+        application_context: {
+          shipping_preference: "NO_SHIPPING",
+        },
+      })
+      .then((orderID) => {
+        setOrderID(orderID);
+        return orderID;
+      });
+  };
+
+  // check Approval
+  const onApprove = (data, actions) => {
+    return actions.order.capture().then(function (details) {
+      const { payer } = details;
+      setSuccess(true);
+    });
+  };
+  //capture likely error
+  const onError = (data, actions) => {
+    setErrorMessage("An Error occured with your payment ");
+  };
+
+  useEffect(() => {
+    if (success) {
+      alert("Payment successful!!");
+    }
+  }, [success]);
+  return (
+    <>
+      <div className="wrapper">
+        <div className="product-img">
+          <img
+            src="https://cdn.pixabay.com/photo/2021/08/15/06/54/sunflower-6546993_1280.jpg"
+            alt="SunFlower"
+            height="420"
+            width="327"
+          />
+        </div>
+        <div className="product-info">
+          <div className="product-text">
+            <h1>Sunflower</h1>
+            <h2>POPULAR HOUSE PLANT</h2>
+            <p>
+              Sunflowers are usually tall annual or perennial plants.
+              <br />
+              Each "flower" is actually a
+              <br />
+              disc made up of tiny flowers,
+              <br />
+              to better attract pollinators.{" "}
+            </p>
+          </div>
+
+          <div className="product-price-btn">
+            <p>
+              <span>$20</span>
+            </p>
+            <button type="submit" onClick={() => setShow(true)}>
+              Buy now
+            </button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <PayPalScriptProvider
+          options={{
+            "client-id": `${process.env.paypal_client_id}`,
+          }}
+        >
+          {show ? (
+            <PayPalButtons
+              createOrder={createOrder}
+              onApprove={onApprove}
+              style={{ layout: "vertical", color: "black" }}
+            />
+          ) : null}
+        </PayPalScriptProvider>
+      </div>
+    </>
+  );
+}
+
+/*
 function Counter(props) {
   function reducer(state, action) {
     switch (action.type) {
@@ -152,4 +256,4 @@ export default function admindashboard() {
       <Counter initialCount={0} />
     </div>
   );
-}
+}*/
