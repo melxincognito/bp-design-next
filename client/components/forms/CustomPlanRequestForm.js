45ea6df07e5bef6e35fbef604deee8e65825b156
Mel Incognito <melxincognito@gmail.com>
2022-07-10 15:44:35 -0700

    took out spaces from formatPhoneNumber function

--- client/components/forms/CustomPlanRequestForm.js
+++ client/components/forms/CustomPlanRequestForm.js
@@ -23,24 +23,18 @@ const Transition = forwardRef(function Transition(props, ref) {
 
 export default function CustomPlanRequestForm() {
   const [openDialog, setOpenDialog] = useState(false);
-
   const [style, setStyle] = useState("Luxury Style");
   const [projectType, setProjectType] = useState("New Construction Project");
   const [phoneInputValue, setPhoneInputValue] = React.useState("");
 
   function formatPhoneNumber(value) {
     if (!value) return value;
-
     const phoneNumber = value.replace(/[^\d]/g, "");
-
     const phoneNumberLength = phoneNumber.length;
-
     if (phoneNumberLength < 4) return phoneNumber;
-
     if (phoneNumberLength < 7) {
       return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
     }
-
     return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
       3,
       6
