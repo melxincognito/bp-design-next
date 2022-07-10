12f7c95e4197ef8ae210f37c12a2f880d91b257a
Mel Incognito <melxincognito@gmail.com>
2022-07-10 14:23:49 -0700

    added styling

--- client/pages/checkout.js
+++ client/pages/checkout.js
@@ -109,7 +109,7 @@ export default function checkout() {
 
   var pricingItems = [
     {
-      label: "Cart",
+      label: "Items Total",
       price: cartTotal,
       color: "primary.main",
     },
@@ -136,54 +136,60 @@ export default function checkout() {
             display: "grid",
             gap: "1rem",
             alignItems: "center",
-            boxShadow: "5px 5px 15px 5px rgba(0,0,0,0.41)",
+            boxShadow: "5px 5px 15px 5px rgba(0,0,0,0.51)",
+            backgroundColor: "primary.light",
           }}
         >
-          <Typography component="h1" variant="h4" align="center">
+          <Typography variant="h4" align="center" color="white">
             Checkout
           </Typography>
-          <hr width="100%" />
 
-          {cartItems.length === 0 ? (
-            <Typography component="div" sx={{ textAlign: "center" }}>
-              Loading cart items . . .
-            </Typography>
-          ) : (
-            <List disablePadding>
-              {cartItems.map((item, index) => (
-                <ListItem key={index} sx={{ py: 1, px: 0 }}>
-                  <ListItemText primary={"Plan #" + item.plan_number} />
-                  <Typography variant="body2">${item.price}</Typography>
-                </ListItem>
-              ))}
-
-              {pricingItems.map((item, index) => {
-                return (
+          <Paper
+            variant="outlined"
+            sx={{
+              padding: 2,
+              boxShadow: "inset 5px 5px 15px 5px rgba(0,0,0,0.27)",
+            }}
+          >
+            {cartItems.length === 0 ? (
+              <Typography component="div" sx={{ textAlign: "center" }}>
+                Loading cart items . . .
+              </Typography>
+            ) : (
+              <List disablePadding>
+                {cartItems.map((item, index) => (
                   <ListItem key={index} sx={{ py: 1, px: 0 }}>
-                    <ListItemText
-                      primary={item.label}
-                      sx={{ fontWeight: 700 }}
-                    />
-                    <Typography
-                      variant="subtitle1"
-                      sx={{ fontWeight: 700, color: `${item.color}` }}
-                    >
-                      {item.price < 0 ? "$0.00" : "$" + `${item.price}`}
-                    </Typography>
+                    <ListItemText primary={"Plan #" + item.plan_number} />
+                    <Typography variant="body2">${item.price}</Typography>
                   </ListItem>
-                );
-              })}
-            </List>
-          )}
+                ))}
 
-          <hr width="100%" />
+                <hr width="100%" height="50%" />
+                {pricingItems.map((item, index) => {
+                  return (
+                    <ListItem key={index} sx={{ py: 1, px: 0 }}>
+                      <ListItemText
+                        primary={item.label}
+                        sx={{ fontWeight: 700 }}
+                      />
+                      <Typography
+                        variant="subtitle1"
+                        sx={{ fontWeight: 700, color: `${item.color}` }}
+                      >
+                        {item.price < 0 ? "$0.00" : "$" + `${item.price}`}
+                      </Typography>
+                    </ListItem>
+                  );
+                })}
+              </List>
+            )}
+          </Paper>
+
           <div
             style={{
               boxShadow: "5px 5px 15px 5px rgba(0,0,0,0.41)",
               borderRadius: "40px",
-              display: "block",
-              alignItems: "center",
-              backgroundColor: "black",
+              backgroundColor: "#470269",
             }}
           >
             <PayPalScriptProvider
