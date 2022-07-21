e114291f4a741a0d8f49ea9d960ee0e0c1b36749
Mel Incognito <melxincognito@gmail.com>
2022-07-21 15:08:29 -0700

    configured firebase and firebase auth

---
+++ client/firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: `${process.env.firebase_api_key}`,
+  authDomain: `${process.env.firebase_auth_domain}`,
+  projectId: `${process.env.firebase_project_id}`,
+  storageBucket: `${process.env.firebase_storage_bucket}`,
+  messagingSenderId: `${process.env.firebase_messaging_sender_id}`,
+  appId: `${process.env.firebase_app_id}`,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
