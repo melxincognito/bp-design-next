53e82e50c3e60c49919d393dbbaae47ca84cb1ae
Mel Incognito <melxincognito@gmail.com>
2022-07-21 15:08:05 -0700

    added env variables for firebase

--- client/next.config.js
+++ client/next.config.js
@@ -10,5 +10,11 @@ module.exports = {
     email_js_template_id: `${process.env.EMAIL_JS_TEMPLATE_ID}`,
     email_js_public_key: `${process.env.EMAIL_JS_PUBLIC_KEY}`,
     paypal_client_id: `${process.env.PAYPAL_CLIENT_ID}`,
+    firebase_api_key: `${process.env.FIREBASE_API_KEY}`,
+    firebase_auth_domain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
+    firebase_project_id: `${process.env.FIREBASE_PROJECT_ID}`,
+    firebase_storage_bucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
+    firebase_messaging_sender_id: `${process.env.FIREBASE_MESSAGING_SENDER_ID}`,
+    firebase_app_id: `${process.env.FIREBASE_APP_ID}`,
   },
 };
