c5be3cdc15a5bcd360fb5cf51dd34737e711d399
Mel Incognito <melxincognito@gmail.com>
2022-07-20 22:21:52 -0700

    added todo instruction

--- client/pages/customplanrequest.js
+++ client/pages/customplanrequest.js
@@ -2,6 +2,8 @@ import React from "react";
 import CustomPlanRequestForm from "../components/forms/CustomPlanRequestForm";
 import { motion } from "framer-motion";
 
+// TODO add instructions above form for how to use form
+
 export default function customplanrequest() {
   return (
     <motion.div
