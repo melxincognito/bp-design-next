53cea1ee5866e44ef555f9fe605e1ef1b1fcda2d
Mel Incognito <melxincognito@gmail.com>
2022-07-21 22:19:56 -0700

    configured environment variable for rt database

--- client/next.config.js
+++ client/next.config.js
@@ -12,6 +12,7 @@ module.exports = {
     paypal_client_id: `${process.env.PAYPAL_CLIENT_ID}`,
     firebase_api_key: `${process.env.FIREBASE_API_KEY}`,
     firebase_auth_domain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
+    firebase_database_url: `${process.env.FIREBASE_DATABASE_URL}`,
     firebase_project_id: `${process.env.FIREBASE_PROJECT_ID}`,
     firebase_storage_bucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
     firebase_messaging_sender_id: `${process.env.FIREBASE_MESSAGING_SENDER_ID}`,
