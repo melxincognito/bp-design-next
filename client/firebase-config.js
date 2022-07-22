271909770de637a7fe4e3c9c19ba9b22c4f01dfb
Mel Incognito <melxincognito@gmail.com>
2022-07-21 22:20:23 -0700

    configured realtime database

--- client/firebase-config.js
+++ client/firebase-config.js
@@ -2,10 +2,12 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: `${process.env.firebase_api_key}`,
   authDomain: `${process.env.firebase_auth_domain}`,
+  databaseURL: `${process.env.firebase_database_url}`,
   projectId: `${process.env.firebase_project_id}`,
   storageBucket: `${process.env.firebase_storage_bucket}`,
   messagingSenderId: `${process.env.firebase_messaging_sender_id}`,
@@ -16,3 +18,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const database = getDatabase(app);
